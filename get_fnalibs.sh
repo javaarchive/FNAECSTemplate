8dfdcb68508a1025b66e3ecbf68815fb9ca54883
Raymond <20248577+javaarchive@users.noreply.github.com>
2025-04-04 11:40:06 -0700

    Update get_fnalibs.sh

--- get_fnalibs.sh
+++ get_fnalibs.sh
@@ -2,10 +2,13 @@
 mkdir -p libs
 cd libs
 
-echo -e "\e[32mDownloading fnalibs...\e[m"
-curl -O https://fna.flibitijibibo.com/archive/fnalibs.tar.bz2
+#echo -e "\e[32mDownloading fnalibs...\e[m"
+#curl -O https://fna.flibitijibibo.com/archive/fnalibs.tar.bz2
 
-echo -e "\e[32mExtracting fnalibs...\e[m"
-tar -xf fnalibs.tar.bz2
+#echo -e "\e[32mExtracting fnalibs...\e[m"
+#tar -xf fnalibs.tar.bz2
+
+echo This script doesn't work anymore.
+echo Go to https://github.com/FNA-XNA/fnalibs-dailies for now.
 
 cd ..
